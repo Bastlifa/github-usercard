a2b15bf768fe169e2f842bec86ee527a3ca83f80
Eli Sacks <bastlifa@gmail.com>
2019-07-11 11:50:35 -0400

    steps 1-4

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,18 +1,29 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+          (replacing the palceholder with your Github name):
+          https://api.github.com/users/<your name>
 */
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
 
-   Skip to Step 3.
+
+axios.get('https://api.github.com/users/Bastlifa')
+  .then (response => document.querySelector('.cards').appendChild(cardMaker(response.data)))
+  .catch( error =>
+    {
+      console.log("Error:", error)
+    })
+
+/* Step 2: Inspect and study the data coming back, this is YOUR 
+  github info! You will need to understand the structure of this 
+  data in order to use it to build your component function 
+
+  Skip to Step 3.
 */
 
 /* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
+    create a new component and add it to the DOM as a child of .cards
 */
+
+
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -46,6 +57,57 @@ const followersArray = [];
 
 */
 
+function cardMaker(userObj)
+{
+  let cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  
+  let usrImg = document.createElement("img");
+  usrImg.src = userObj.avatar_url;
+  cardDiv.appendChild(usrImg);
+
+  let cardInfo = document.createElement("div")
+  cardInfo.classList.add("card-info");
+  cardDiv.appendChild(cardInfo);
+
+  let nameH3 = document.createElement("h3");
+  nameH3.classList.add("name");
+  nameH3.textContent = "Eli Sacks";
+  cardInfo.appendChild(nameH3);
+
+  let usrNameP = document.createElement("p");
+  usrNameP.classList.add("username");
+  usrNameP.textContent = userObj.login;
+  cardInfo.appendChild(usrNameP);
+
+  let usrLocP = document.createElement("p");
+  usrLocP.textContent = `Location: ${userObj.location}`;
+  cardInfo.appendChild(usrLocP);
+
+  let usrProfA = document.createElement("a");
+  usrProfA.href = "https://github.com/Bastlifa";
+  usrProfA.textContent = "https://github.com/Bastlifa"
+
+  let usrProfP = document.createElement("p");
+  usrProfP.textContent = `Profile: `;
+  usrProfP.appendChild(usrProfA)
+  cardInfo.appendChild(usrProfP);
+
+  let usrFollowerCountP = document.createElement("p");
+  usrFollowerCountP.textContent =`Followers: ${userObj.followers}`;
+  cardInfo.appendChild(usrFollowerCountP);
+
+  let usrFollowingCountP = document.createElement("p");
+  usrFollowingCountP.textContent = `Following ${userObj.following}`;
+  cardInfo.appendChild(usrFollowingCountP);
+
+  let usrBioP = document.createElement("p");
+  usrBioP.textContent = `Bio: ${userObj.bio}`;
+  cardInfo.appendChild(usrBioP);
+
+  return cardDiv
+}
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
@@ -53,3 +115,4 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
